d0eac8208a0962c6e5619cd70c1f1094f2252349
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-01-06 08:43:43 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,6 +1,6 @@
 def solution(num_list, n):
     result = []
-    for i in range(len(num_list)//n):
-        result.append([j for j in num_list[i*n:(i+1)*n]])
+    for i in range(0, len(num_list), n):
+        result.append(num_list[i:i+n])
         
     return result
