1c14f00b2281fcc35ee25dba967cd53ff9cb1830
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-01-09 16:11:52 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,9 @@
+def solution(id_pw, db):
+    for i in db:
+        if id_pw == i:
+            return "login"
+        elif id_pw[0] == i[0] and id_pw[1] != i[1]:
+            return "wrong pw"
+        
+    return "fail"
+            
