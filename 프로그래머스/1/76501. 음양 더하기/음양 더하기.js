94e9b9c8d6613c6e01fe9e09635200393b18cf60
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-05-01 09:29:07 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,8 @@
 function solution(absolutes, signs) {
     let result = 0;
     for (let i = 0; i < signs.length; i++) {
-        if (signs[i]) {
-            result += absolutes[i];
-        } else {
-            result -= absolutes[i];
-        }
+        if (signs[i]) result += absolutes[i];
+        else result -= absolutes[i];
     }
     
     return result
