c930f28dd2a0959914755e35d96725a8e7f848de
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-05-27 09:21:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,18 +1,17 @@
 function solution(nums) {
     let result = 0;
+    let temp = 0;
     let isPrime = true;
     
     for (let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j++) {
             for (let k = j + 1; k < nums.length; k++) {
+                temp = nums[i] + nums[j] + nums[k];
                 isPrime = true;
-                for (let l = 2; l < nums[i] + nums[j] + nums[k]; l++) {
-                    if ((nums[i] + nums[j] + nums[k]) % l === 0)
-                        isPrime = false;
+                for (let l = 2; l < Math.floor(temp / 2); l++) {
+                    if (temp % l === 0) isPrime = false;
                 }
-                if (isPrime){
-                    result++;
-                }
+                if (isPrime) result++;
             }
         }
     }
