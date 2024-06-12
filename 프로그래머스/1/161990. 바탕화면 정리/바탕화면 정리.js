85d9b16af805ba819b71852051d6f7b55ac43d3a
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-06-12 09:23:50 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,20 +1,4 @@
 function solution(wallpaper) {
-    let minLux = 99, minLuy = 99;
-    let maxRdx = 0, maxRdy = 0;
-    
-    const width = wallpaper[0].length;
-    const height = wallpaper.length;
-    
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            if (wallpaper[i][j] == '#') {
-                minLux = Math.min(minLux, i);
-                maxRdx = Math.max(maxRdx, i);
-                minLuy = Math.min(minLuy, j);
-                maxRdy = Math.max(maxRdy, j);
-            }
-        }
-    }
-    
-    return [minLux, minLuy, maxRdx+1, maxRdy+1];
+    var answer = [];
+    return answer;
 }
