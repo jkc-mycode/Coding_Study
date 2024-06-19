867b3dfe379812d2c759b1fff8043352ee5219d6
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-06-19 09:16:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -6,7 +6,9 @@ function solution(s) {
         if (!isNaN(strArr[i][0])) continue;
         
         strArr[i] = strArr[i].split("");
+        // 공백이 연속일 때는 strArr[i]의 길이는 0
         if (strArr[i].length !== 0)
+            // 공백은 toUpperCase()를 못하기 때문에 런타임에러 발생
             strArr[i][0] = strArr[i][0].toUpperCase();
         strArr[i] = strArr[i].join("");
     }
