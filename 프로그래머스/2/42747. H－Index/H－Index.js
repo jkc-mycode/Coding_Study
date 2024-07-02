b752996a2db6b55e763afd40950b15a386ea1a71
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-07-02 09:33:12 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,16 @@
+function solution(citations) {
+    let use;
+    let etc;
+    let max = -99;
+    for (let i = 0; i <= Math.max(...citations); i++) {
+        use = citations.filter((val) => val >= i);
+        etc = citations.filter((val) => val <= i);
+        
+        if (use.length >= i) {
+            max = i;
+        } else {
+            break;
+        }
+    }
+    return max;
+}
