d52f52919075019ade46900f097d69fe8d3914a3
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2024-07-10 09:53:46 +0900

    [level 2] Title: 프로세스, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,20 @@
 function solution(priorities, location) {
-    const test = [];
+    const queue = [];
     for (let i = 0; i < priorities.length; i++) {
-        test.push([i, priorities[i]]);
+        queue.push([i, priorities[i]]);
     }
     
     let count = 0;
     
     while(true) {
-        const max = test.reduce((a, b) => a[1] > b[1] ? a : b)[1];
+        const max = queue.reduce((a, b) => a[1] > b[1] ? a : b)[1];
         
         let shift;
-        if (test[0][1] < max) {
-            const temp = test.shift();
-            test.push(temp);
-        } else if (test[0][1] === max) {
+        if (queue[0][1] < max) {
+            queue.push(queue.shift());
+        } else if (queue[0][1] === max) {
             count++;
-            shift = test.shift();
+            shift = queue.shift();
         }
         
         if (shift && shift[0] === location) {
