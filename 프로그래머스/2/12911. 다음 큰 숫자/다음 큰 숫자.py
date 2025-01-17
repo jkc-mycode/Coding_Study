64d5a2e40381916ac358354699dd0920fc4b5464
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-01-17 14:35:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,12 @@
+def solution(n):
+    origin_num = n
+    origin_bin_num = bin(n)[2:]
+    
+    while True:
+        n += 1
+        bin_num = bin(n)[2:]
+        if origin_bin_num.count('1') == bin_num.count('1'):
+            break
+    
+    return n
+        
