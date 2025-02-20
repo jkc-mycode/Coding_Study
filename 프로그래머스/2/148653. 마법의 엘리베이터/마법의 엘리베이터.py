ceecc6a91413f4c37a119ff16b030b756abdd9e7
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-02-20 16:05:13 +0900

    Update 마법의 엘리베이터.py

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -3,21 +3,13 @@ def solution(storey):
     c = len(str(storey)) - 1
     
     while storey > 0:
+        if storey <= 5:
+            result += storey
+            break
         div = storey // pow(10, c)
-        if div > 5:
+        if div > 5 or div == 5 and int(str(storey)[1]) >= 5:
             storey = pow(10, c+1) - storey
             result += 1
-        elif div == 5:
-            if storey == 5:
-                result += 5
-                break
-            elif int(str(storey)[1]) >= 5:
-                storey = pow(10, c+1) - storey
-                result += 1
-            else:
-                storey = storey % pow(10, c)
-                c -= 1
-                result += div 
         else:
             storey = storey % pow(10, c)
             c -= 1
