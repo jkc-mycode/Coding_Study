a6fa356302a6474764965191b92297a969302c9f
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-02-26 13:36:55 +0900

    [level 2] Title: 숫자의 표현, Time: 5.30 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -2,14 +2,12 @@ def solution(n):
     result = 0
     
     for i in range(1, n+1):
-        cur_sum = 0
+        num = 0
         for j in range(i, n+1):
-            cur_sum += j
-            if cur_sum == n:
-                result += 1
+            num += j
+            if num >= n:
                 break
-            
-            if cur_sum > n:
-                break
-        
+        if num == n:
+            result += 1
+    
     return result
