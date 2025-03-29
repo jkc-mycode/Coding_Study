b522714e4dba5014ffd18ff4808dc0342e38908c
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-03-29 15:56:51 +0900

    [level 2] Title: 튜플, Time: 407.11 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,13 +1,10 @@
-import json
-
 def solution(s):
-    formated_str = s.replace('{','[').replace('}',']')
-    formated_list = list(map(list, json.loads(formated_str)))
-    formated_list.sort(key=len)
-    result = [formated_list[0][0]]
+    s = sorted([i.split(',') for i in s[2:-2].split('},{')], key=lambda x: len(x))
+    result = [int(s[0][0])]
     
-    for i in range(len(formated_list)-1):
-        diff = [j for j in formated_list[i+1] if j not in formated_list[i]]
-        result += diff
-        
+    for i in range(1, len(s)):
+        temp = [value for value in s[i] if value not in s[i-1]]
+        result.append(int(temp[0]))
+    
     return result
+            
