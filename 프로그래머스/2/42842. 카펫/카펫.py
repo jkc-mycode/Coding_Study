934f6e77340cb6299b4a6ec3123b631e8b9fce10
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-05-19 21:44:42 +0900

    [level 2] Title: 카펫, Time: 39.23 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,15 @@
 def solution(brown, yellow):
     total = brown + yellow
+    result = []
+    for i in range(3, total//2):
+        if total % i == 0:
+            result.append(total // i)
     
-    for y in range(3, int(total**0.5)+2):
-        if total % y == 0:
-            x = total // y
-            if x >= y and (x-2) * (y-2) == yellow:
-                return [x, y]
+    for y in result:
+        x = total // y
+        if x >= y and (x-2) * (y-2) == yellow:
+            return [x, y]
+    
+        
+            
+        
