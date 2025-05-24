8780286e613ae2a3413ffc584072f9f8ea7dd3a2
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-05-24 16:00:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,18 +1,18 @@
+import math
+
 def solution(n, words):
+    words_dict = {words[0]: 1}
     result = [0, 0]
-    i = 1
-    words_dict = {w: 0 for w in words}
     
-    while i < len(words):
-        words_dict[words[i-1]] += 1
-        if words[i-1][-1] != words[i][0] or words_dict[words[i]] != 0:
-            result[0] = i % n
-            result[1] = i // n
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words_dict.get(words[i]):
+            result[0] = (i + 1) % n if (i + 1) % n != 0 else n
+            result[1] = math.ceil((i + 1) / n)
             break
-        i += 1
-    
-    if result[0] != 0 or result[1] != 0:
-        result[0] += 1
-        result[1] += 1
+            
+        if words_dict.get(words[i]):
+            words_dict[words[i]] += 1
+        else:
+            words_dict[words[i]] = 1
     
     return result
