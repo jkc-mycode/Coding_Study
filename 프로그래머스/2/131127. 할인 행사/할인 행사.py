ce51ec25c9969c9b9c0fe3f1b2db67e03bd585df
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-05-24 17:38:29 +0900

    Update 할인 행사.py

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -6,14 +6,9 @@ def solution(want, number, discount):
     
     for i in range(len(discount)-9):
         discount_dict = dict(Counter(discount[i:i+10]))
-        flag = True
         
-        for w in want:
-            if discount_dict.get(w) == None or discount_dict[w] < want_dict[w]:
-                flag = False
-                break
-                
-        if flag: result += 1
+        if discount_dict == want_dict:
+            result += 1
     
     return result
     
