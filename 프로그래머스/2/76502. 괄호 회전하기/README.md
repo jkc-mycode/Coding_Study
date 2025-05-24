0d9731b2ed99f5e464655b5cf934b84f69e81a8d
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-05-24 18:10:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 117.66 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 343.34 ms
+메모리: 9.43 MB, 시간: 117.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 15:19:11
+2025년 05월 24일 18:10:20
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,22 @@
-def is_right(s):
-    s_dict = {'(': ')', '[': ']', '{': '}'}
-    stack = []
-    
-    for i in s:
-        if len(stack) == 0:
-            stack.append(i)
-        else:
-            if s_dict.get(stack[-1]) and s_dict[stack[-1]] == i:
-                stack.pop()
-            else:
-                stack.append(i)
-    
-    if len(stack) == 0:
-        return True
-    else:
-        return False
-
 def solution(s):
     result = 0
+    dict_s = { ')': '(', ']': '[', '}': '{' }
+    opened_s = ['(', '[', '{']
+    closed_s = [')', ']', '}']
+    
     for i in range(len(s)):
         new_s = s[i:] + s[:i]
-        if is_right(new_s):
+        
+        if new_s[0] in closed_s: continue
+        
+        stack = []
+        for n in new_s:
+            if len(stack) == 0 or n in opened_s:
+                stack.append(n)
+            elif dict_s[n] == stack[-1]:
+                stack.pop()
+        
+        if len(stack) == 0:
             result += 1
     
     return result
