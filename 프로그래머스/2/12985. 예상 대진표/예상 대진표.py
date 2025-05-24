856ea8e3f9b5066fb0b7b8145574f02bcc25cab1
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-05-24 16:34:42 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,2 @@
 def solution(n,a,b):
-    result = 0
-    
-    while a != b:
-        a = (a + 1) // 2
-        b = (b + 1) // 2
-        result += 1
-    
-    return result
+    return ((a - 1)^(b - 1)).bit_length()
