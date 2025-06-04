548c603db23f3dcd2db5f3acc55e2e8d285e8836
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-06-04 11:45:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.04 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,23 @@
 from collections import deque
 
+# BFS 활용
 def solution(maps):
-    row, col = len(maps), len(maps[0])
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    queue = deque([(0, 0)])
-    dist = [[0] * col for _ in range(row)]
-    dist[0][0] = 1
+    n, m = len(maps), len(maps[0])
+    queue = deque([(0, 0, 1)])
+    dist = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    visited = [[False] * m for _ in range(n)]
+    visited[0][0] = True
     
     while queue:
-        x, y = queue.popleft()
+        x, y, value = queue.popleft()
         
-        for dx, dy in directions:
-            nx, ny = dx + x, dy + y
+        if x == n-1 and y == m-1:
+            return value
+        
+        for d in dist:
+            nx, ny = x + d[0], y + d[1]
             
-            if 0 <= nx < row and 0 <= ny < col and maps[nx][ny] == 1 and dist[nx][ny] == 0:
-                dist[nx][ny] = dist[x][y] + 1
-                queue.append((nx, ny))
-    
-    if dist[row-1][col-1] == 0:
-        return -1
-    else:
-        return dist[row-1][col-1]
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and maps[nx][ny] == 1:
+                visited[nx][ny] = True
+                queue.append((nx, ny, value+1))
+    return -1
