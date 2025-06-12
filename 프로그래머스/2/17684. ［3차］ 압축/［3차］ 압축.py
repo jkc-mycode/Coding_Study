43fb1ef0ca85c9562c160117b99cfe9d524f8a1a
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-06-12 11:58:41 +0900

    [level 2] Title: [3차] 압축, Time: 1.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,19 +1,20 @@
 def solution(msg):
     result = []
-    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    dic = {alpha[i]: i+1 for i in range(len(alpha))}
-    count = len(dic) + 1
+    words = {chr(65+i): i+1 for i in range(26)}
+    count = 27
     
     while True:
-        if dic.get(msg) != None:
-            result.append(dic[msg])
+        if words.get(msg) != None:
+            result.append(words[msg])
             break
+        
         for i in range(1, len(msg)+1):
-            if dic.get(msg[0:i]) == None:
-                result.append(dic[msg[0:i-1]])
-                dic[msg[0:i]] = count
+            if words.get(msg[0:i]) == None:
+                result.append(words[msg[0:i-1]])
+                words[msg[0:i]] = count
                 count += 1
                 msg = msg[i-1:]
                 break
-                
+        
     return result
+        
