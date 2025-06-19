5775ec3780872d1bca7a3228fffb888e31977ffe
jkc-mycode <90839233+jkc-mycode@users.noreply.github.com>
2025-06-19 11:10:12 +0900

    [level 2] Title: 타겟 넘버, Time: 462.07 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,15 @@
-def solution(numbers, target):
-    result = 0
-    def dfs(index, cur_sum):
-        nonlocal result
-        if index == len(numbers):
-            if cur_sum == target:
-                result += 1
-            return 
-        
-        dfs(index + 1, cur_sum + numbers[index])
-        dfs(index + 1, cur_sum - numbers[index])
+result = 0
+def dfs(index, num_sum, numbers, target):
+    global result
+    if index == len(numbers) and num_sum == target:
+        result += 1
+        return 
+    if index == len(numbers):
+        return
     
-    dfs(0, 0)
+    dfs(index + 1, num_sum + numbers[index], numbers, target)
+    dfs(index + 1, num_sum - numbers[index], numbers, target)
+
+def solution(numbers, target):
+    dfs(0, 0, numbers, target)
     return result
